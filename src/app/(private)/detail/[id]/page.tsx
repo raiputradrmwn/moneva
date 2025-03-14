3d3dd6df70aeeb2e39955378badcbb7256e55b6d
Hanntuu <raihanrrdarmawan@gmail.com>
2025-03-14 18:53:02 +0700

    fix (detail) : fix pageprops

--- src/app/(private)/detail/[id]/page.tsx
+++ src/app/(private)/detail/[id]/page.tsx
@@ -1,11 +1,11 @@
+import { use } from "react";
 import DetailContent from "../components/DetailContent";
-
-interface DetailPageProps {
-  params: {
-    id: string;
-  };
-}
-
-export default function DetailPage({ params }: DetailPageProps) {
-  return <DetailContent id={params.id} />;
+    
+export default function CategoryDetail({params}: {params: Promise<{ id: string }>}) {
+const { id } = use(params);
+  return (
+    <>
+      <DetailContent  id={id} />
+    </>
+  );
 }
